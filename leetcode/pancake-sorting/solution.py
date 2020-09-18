5333d65af84807ee857be942c62c5fd411b102b2
Michał Linek <michal.linek89@gmail.com>
2020-09-18 15:02:58 +0200

    pancake-sorting solution

---
+++ leetcode/pancake-sorting/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        if len(arr) == 0: return []
+        operations = []
+        highest_num_id = len(arr) - 1
+        while highest_num_id > 0:
+            if arr[highest_num_id] != highest_num_id + 1:
+                i = highest_num_id
+                id = arr.index(highest_num_id + 1)
+                
+                operations.append(id + 1)
+                arr = arr[id::-1] + arr[id + 1:]
+                operations.append(highest_num_id + 1)
+                arr = arr[highest_num_id::-1] + arr[highest_num_id + 1:]
+                
+            highest_num_id -= 1
+            
+        return operations
+        
